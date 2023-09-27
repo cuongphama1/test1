fa4dbf5da7b42759119d7b8c11ba24f00d8625b2
cuongphama1 <74636566+cuongphama1@users.noreply.github.com>
2023-09-27 23:38:14 +0700

    Update PrimeCheckerTest.java

--- PrimeCheckerTest.java
+++ PrimeCheckerTest.java
@@ -1,34 +1,38 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class PrimeCheckerTest {
 
     @Test
-    public void testNegativeNumbersAndZero() {
+    void testIsPrime() {
+        // Kiểm tra số -5, kết quả mong đợi là False
         assertFalse(PrimeChecker.isPrime(-5));
+
+        // Kiểm tra số 0, kết quả mong đợi là False
         assertFalse(PrimeChecker.isPrime(0));
-    }
 
-    @Test
-    public void testOne() {
+        // Kiểm tra số 1, kết quả mong đợi là False
         assertFalse(PrimeChecker.isPrime(1));
-    }
 
-    @Test
-    public void testPrimes() {
+        // Kiểm tra số 2, kết quả mong đợi là True
         assertTrue(PrimeChecker.isPrime(2));
-        assertTrue(PrimeChecker.isPrime(3));
-        assertTrue(PrimeChecker.isPrime(5));
-        assertTrue(PrimeChecker.isPrime(7));
-        assertTrue(PrimeChecker.isPrime(11));
-    }
 
-    @Test
-    public void testNonPrimes() {
+        // Kiểm tra số 3, kết quả mong đợi là True
+        assertTrue(PrimeChecker.isPrime(3));
+
+        // Kiểm tra số 4, kết quả mong đợi là False
         assertFalse(PrimeChecker.isPrime(4));
-        assertFalse(PrimeChecker.isPrime(6));
-        assertFalse(PrimeChecker.isPrime(8));
-        assertFalse(PrimeChecker.isPrime(9));
-        assertFalse(PrimeChecker.isPrime(10));
+
+        // Kiểm tra số 5, kết quả mong đợi là True
+        assertTrue(PrimeChecker.isPrime(5));
+
+        // Kiểm tra số 17, kết quả mong đợi là True
+        assertTrue(PrimeChecker.isPrime(17));
+
+        // Kiểm tra số 20, kết quả mong đợi là False
+        assertFalse(PrimeChecker.isPrime(20));
+
+        // Kiểm tra số 97, kết quả mong đợi là True
+        assertTrue(PrimeChecker.isPrime(97));
     }
 }
